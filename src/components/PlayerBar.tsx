a2d318d529cdc7ad091eeb33d727db3b91fa7969
Neppex <cignomusic@gmail.com>
2026-05-22 20:43:15 +0200

    Still unstable progress bars
    Opencode thought the problem was the frontend wasn't syncing everything together but that doesn't seem to help.

--- src/components/PlayerBar.tsx
+++ src/components/PlayerBar.tsx
@@ -102,6 +102,7 @@ export default function PlayerBar({ selectedTrack, onNext, onPrev }: PlayerBarPr
       .then((data) => {
         waveRef.current = data.peaks;
         if (data.duration > 0) {
+          setStatus((prev) => ({ ...prev, duration: data.duration }));
           invoke('set_duration', { duration: data.duration });
         }
       })
